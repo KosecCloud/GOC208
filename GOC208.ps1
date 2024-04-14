1a56b5308318744b1d8d578d90af2c4abac4b9a0
Kosec Solutions, s.r.o <166837726+KosecCloud@users.noreply.github.com>
2024-04-14 08:58:03 +0200

    Add files via upload

---
+++ GOC208.ps1
@@ -0,0 +1,20 @@
+﻿Get-FileHash
+
+
+
+# Kontrola HASHi OS
+Get-FileHash E:\kurz\22621.525.220925-0207.ni_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso
+
+certutil -hashfile SHA256 C:\Test\*.iso
+
+# Odebrani UWA:
+Get-AppxPackage *zune* | Remove-AppxPackage
+
+Get-Module -ListAvailable
+Import-Module DISM
+Get-Module
+Get-Command -Module DISM | ogv
+
+Update-Help
+help Mount-WindowsImage -Examples
+Dismount-WindowsImage –Path "e:\kurz\W11Export" -save
